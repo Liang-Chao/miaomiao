cb360566ef9c1aac2f3430501f67bb04dae97704
LiangCh <412611626@qq.com>
2020-09-06 14:13:26 +0800

    0906createComponents

---
+++ src/views/Cinema/index.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="main">
+    <Header title="喵喵影院" />
+    <div id="content">
+      <div class="cinema_menu">
+        <div class="cinema_switch">
+          全城 <i class="iconfont">&#xe74e;</i>
+        </div>
+        <div class="cinema_switch">
+          品牌 <i class="iconfont">&#xe74e;</i>
+        </div>
+        <div class="cinema_switch">
+          特色 <i class="iconfont">&#xe74e;</i>
+        </div>
+      </div>
+      <CinemaList />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import CinemaList from '@/components/CinemaList'
+
+export default {
+  name: 'Cinema',
+  components: {
+    Header,
+    Footer,
+    CinemaList
+  }
+}
+</script>
+
+<style scoped>
+#content .cinema_menu {
+  width: 100%;
+  height: 45px;
+  border-bottom: 1px solid #e6e6e6;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background: white;
+}
+.cinema_menu .cinema_switch {
+  display: flex;
+  height: 100%;
+  line-height: 45px;
+}
+</style>
---
+++ src/views/Mine/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="main">
+    <Header title="我的喵喵" />
+    <div id="content">
+      <Login />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/Header'
+import Footer from '@/components/Footer'
+import Login from '@/components/Login'
+
+export default {
+  name: 'Mine',
+  components: {
+    Header,
+    Footer,
+    Login
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
